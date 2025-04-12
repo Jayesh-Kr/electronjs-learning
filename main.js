da6ca2cb6afea41ff5c3ed93ffd8fe6e7b20ba61
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-04-13 02:39:44 +0530

    Background run & Quit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const {app,BrowserWindow, ipcMain, nativeImage, Tray, Menu} = require("electron");
 const path = require("path");
-
+let win;
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width : 800,
         height : 600,
         webPreferences : {
@@ -10,7 +10,15 @@ const createWindow = () => {
         }
     });
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+    win.on('close', (e) => {
+        if(!app.isQuitting) {
+        e.preventDefault();
+        win.hide(); // Hide window
+        } else {
+            win = null;
+        }
+    })
 }
 
 let tray;
@@ -21,20 +29,20 @@ app.whenReady().then(()=>{
     tray = new Tray(icon);
 
     const contextMenu = Menu.buildFromTemplate([
-        {label : 'Item1', type : 'radio'},
-        {label : 'Item2', type : 'radio'},
-        {label : 'Item3', type : 'radio', checked : true},
-        {label : 'Item4', type : 'radio'},
+        {label : 'Show-App' , click : () => win.show()},
+        {label : 'Quit' , click : () => {app.isQuitting = true; tray.destroy(); app.quit();}}
     ])
     tray.setContextMenu(contextMenu);
     tray.setToolTip("This is my Application")
     tray.setTitle("This is my title")
     createWindow();
+
     app.on('activate' , () => {
         if(BrowserWindow.getAllWindows.length === 0) createWindow();
     })
 }).catch((err)=> { console.log("Error while starting electron"); console.log(err) });
 
-app.on('window-all-closed' , ()=> {
+app.on('window-all-closed' , (e)=> {
+    // e.preventDefault();
     if(process.platform !== 'darwin') app.quit();
 })
