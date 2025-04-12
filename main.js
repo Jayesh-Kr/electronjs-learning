c27fe5cba000638a64a7feb68f4b6d1a30cbe900
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-04-13 02:05:34 +0530

    Added tray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app,BrowserWindow, ipcMain} = require("electron");
+const {app,BrowserWindow, ipcMain, nativeImage, Tray, Menu} = require("electron");
 const path = require("path");
 
 const createWindow = () => {
@@ -13,10 +13,22 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
+let tray;
 app.whenReady().then(()=>{
 
     ipcMain.handle('ping' , () => 'pong');
+    const icon = nativeImage.createFromPath('img.jpeg');
+    tray = new Tray(icon);
 
+    const contextMenu = Menu.buildFromTemplate([
+        {label : 'Item1', type : 'radio'},
+        {label : 'Item2', type : 'radio'},
+        {label : 'Item3', type : 'radio', checked : true},
+        {label : 'Item4', type : 'radio'},
+    ])
+    tray.setContextMenu(contextMenu);
+    tray.setToolTip("This is my Application")
+    tray.setTitle("This is my title")
     createWindow();
     app.on('activate' , () => {
         if(BrowserWindow.getAllWindows.length === 0) createWindow();
